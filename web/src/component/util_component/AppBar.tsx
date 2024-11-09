7cd33a0897046324a1827d12b343a33cd5316328
AyushMahawar <ayushmahawar60@gmail.com>
2024-11-10 03:38:59 +0530

    add logout

--- web/src/component/util_component/AppBar.tsx
+++ web/src/component/util_component/AppBar.tsx
@@ -1,7 +1,10 @@
 import { jwtDecode } from "jwt-decode";
 import { DecodedToken } from "../../utils/CustomInterfaces";
+import { CustomButton } from "./CustomButtonComponent";
+import { useNavigate } from "react-router-dom";
 
 export function AppBar() {
+  const navigate = useNavigate();
   const user = jwtDecode<DecodedToken>(localStorage.getItem("token")!);
   const id = user.email;
 
@@ -13,8 +16,17 @@ export function AppBar() {
           EventManagement
         </div>
 
-        <div className="text-slate-100 rounded-full bg-slate-600 px-1">
-          <p className="p-2">{firstTwo}</p>
+        <div className="flex justify-center items-center">
+          <div className="text-slate-100 rounded-full bg-slate-600 px-1 mx-2">
+            <p className="p-2">{firstTwo}</p>
+          </div>
+          <CustomButton
+            title="Logout"
+            onTap={() => {
+              localStorage.removeItem("token");
+              navigate("/user/signin");
+            }}
+          />
         </div>
       </div>
       <hr className="h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
